b8dcf9ecb067f69aee69f7be6b49ee0d3a32ffb0
e093366 <donald.kostello@mastercard.com>
2023-12-07 12:34:42 -0600

    Trebuchet problem - part 2

--- src/main/java/Trebuchet.java
+++ src/main/java/Trebuchet.java
@@ -3,6 +3,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+/**
+ * The newly-improved calibration document consists of lines of text; each line originally contained a specific
+ * calibration value that the Elves now need to recover. On each line, the calibration value can be found by
+ * combining the first digit and the last digit (in that order) to form a single two-digit number.
+ * <p>
+ * For example:
+ * <p>
+ * 1abc2
+ * pqr3stu8vwx
+ * a1b2c3d4e5f
+ * treb7uchet
+ * In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces
+ * 142.
+ * <p>
+ * Consider your entire calibration document. What is the sum of all of the calibration values?
+ */
 public class Trebuchet {
     public static void main(String[] args) {
         try {
@@ -20,7 +36,7 @@ public class Trebuchet {
                         break;
                     }
                 }
-                for (int i = line.length() -1; i >= 0; i--) {
+                for (int i = line.length() - 1; i >= 0; i--) {
                     if (Character.isDigit(line.charAt(i))) {
                         number += line.charAt(i);
                         break;
---
+++ src/main/java/Trebuchet2.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one,
+ * two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+ * <p>
+ * Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+ * <p>
+ * two1nine
+ * eightwothree
+ * abcone2threexyz
+ * xtwone3four
+ * 4nineeightseven2
+ * zoneight234
+ * 7pqrstsixteen
+ * In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+ * <p>
+ * What is the sum of all of the calibration values?
+ */
+public class Trebuchet2 {
+    public static void main(String[] args) {
+        Map<String, String> numberWords = new HashMap<>();
+        numberWords.put("one", "1");
+        numberWords.put("two", "2");
+        numberWords.put("three", "3");
+        numberWords.put("four", "4");
+        numberWords.put("five", "5");
+        numberWords.put("six", "6");
+        numberWords.put("seven", "7");
+        numberWords.put("eight", "8");
+        numberWords.put("nine", "9");
+        try {
+            // Shouldn't need the full path, not sure why
+            Path path = Paths.get("src/main/resources/Trebuchet.txt");
+            Scanner scanner = new Scanner(path);
+            int total = 0;
+            while (scanner.hasNextLine()) {
+                //process each line
+                String line = scanner.nextLine();
+                String number = "";
+                int numberIndex = -1;
+                String numberToAdd = "";
+                // Find the first digit
+                for (int i = 0; i < line.length(); i++) {
+                    if (Character.isDigit(line.charAt(i))) {
+                        numberIndex = i;
+                        numberToAdd = "" + line.charAt(i);
+                        break;
+                    }
+                }
+                // Find the first word that's a digit - use it if it's before a digit
+                for (String numberString : numberWords.keySet()) {
+                    int i = line.indexOf(numberString);
+                    if (i >= 0 && i < numberIndex) {
+                        numberIndex = i;
+                        numberToAdd = numberWords.get(numberString);
+                    }
+                }
+                number += numberToAdd;
+
+                // Find the last digit
+                for (int i = line.length() - 1; i >= 0; i--) {
+                    if (Character.isDigit(line.charAt(i))) {
+                        numberIndex = i;
+                        numberToAdd = "" + line.charAt(i);
+                        break;
+                    }
+                }
+                // Find the first word that's a digit - use it if it's after a digit
+                for (String numberString : numberWords.keySet()) {
+                    int i = line.lastIndexOf(numberString);
+                    if (i >= 0 && i > numberIndex) {
+                        numberIndex = i;
+                        numberToAdd = numberWords.get(numberString);
+                    }
+                }
+                number += numberToAdd;
+                total += Integer.parseInt(number);
+                System.out.println(line + " - " + number);
+            }
+            scanner.close();
+            System.out.println("Total - " + total);
+        } catch (IOException e) {
+            System.out.println("Caught exception - " + e);
+        }
+    }
+}
